879aa727b1daa2a78e462470013d432a9215a90f
Hack Reactor Students <accounts@hackreactor.com>
2015-08-01 11:08:59 -0700

    Implemented custom directive for links display.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -7,4 +7,16 @@ angular.module('shortly.links', [])
     $scope.data = linkArray;
     console.log("Links.getLinks: ", $scope.data);
   });
+})
+.directive('ngLink', function() {
+  return {
+    restrict: 'A',
+    template: '<img src="assets/redirect_icon.png"/> \
+      <div class="info"> \
+        <div class="visits"><span class="count">{{link.visits}}</span>Visits</div> \
+        <div class="title">{{link.title}}</div> \
+        <div class="original">{{link.url}}</div> \
+        <a href="{{link.base_url}}/api/links/{{link.code}}">{{link.base_url}}/{{link.code}}</a> \
+      </div>'
+  }
 });
